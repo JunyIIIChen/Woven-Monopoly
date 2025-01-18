825c75553ddaf94f6d9f0a3fe7ee7529ac3fd725
JunyIIIChen <cjy799255537@gmail.com>
2025-01-18 16:10:05 +1100

    after analyzing requirement create initial class diagram

--- Player.rb
+++ Player.rb
@@ -1,8 +1,7 @@
 class Player
   
-  attr_accessor :name
+  attr_accessor :name, :money, :position, :properties
 
-  
   def initialize(name)
     @name = name
     @money = 16
@@ -10,4 +9,24 @@ class Player
     @properties = []
   end
 
+  def move(steps, board_size)
+    @position = (@position + steps) % board_size
+    @money += 1 if @position < steps # Pass GO
+  end
+
+  def buy_property(property)
+    return unless @money >= property.price
+    @money -= property.price
+    @properties << property
+    property.owner = self
+  end
+
+  def pay_rent(amount)
+    @money -= amount
+  end
+
   
+  def is_bankrupt?
+    @money < 0
+  end
+end
--- README.md
+++ README.md
@@ -15,7 +15,11 @@ In Woven Monopoly, when the dice rolls are set ahead of time, the game is determ
 * You get $1 when you pass GO (this excludes your starting move)
 * If you land on a property, you must buy it
 * If you land on an owned property, you must pay rent to the owner
+
+**********
 * If the same owner owns all property of the same colour, the rent is doubled
+**********
+
 * Once someone is bankrupt, whoever has the most money remaining is the winner
 * There are no chance cards, jail or stations
 * The board wraps around (i.e. you get to the last space, the next space is the first space)
