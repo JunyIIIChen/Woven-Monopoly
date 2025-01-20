5e1b3ade377e9d09fcd74a34337624591fc929d6
JunyIIIChen <cjy799255537@gmail.com>
2025-01-20 20:59:17 +1100

    improve design and develop main game functions

--- Monopoly.rb
+++ Monopoly.rb
@@ -1,7 +1,10 @@
 require 'json'
+require_relative 'Player'
+require_relative 'Board'
+require_relative 'Property'
 
 class Monopoly
-  attr_accessor :players, :board, :rolls
+  attr_accessor :players, :board, :dice_rolls
 
   def initialize(board_file,roll_dice_file)
     @players = [
@@ -11,16 +14,28 @@ class Monopoly
       Player.new("Sweedal")
     ]
 
-    @board = Board.load_board("board.json")
+    @board = Board.new("board.json")
+
+    @dice_rolls = load_dice_rolls("rolls_1.json")
 
   end
 
+
+  def load_dice_rolls(file)
+    rolls = JSON.parse(File.read(file))
+    puts rolls  # Print the dice rolls
+    rolls       # Return the parsed rolls
+  end
 
   def game_start
     #
+
+    return print_winner if current_player.is_bankrupt?
+
   end
 
-  def take_turn
+  def take_turn_to_move(player,roll)
+
     #
   end
 
@@ -37,8 +52,17 @@ class Monopoly
     #
   end
 
-  def check_winner
-    # puts "Gave Over the winner is #{player.name}"
+  def print_winner()
+    puts "Gave Over the winner is #{player.name}"
+
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  board_file = 'board.json'  # Path to the board file
+  dice_rolls_file = 'rolls_1.json'  # Path to the dice rolls file
+
+  game = Monopoly.new(board_file, dice_rolls_file)
+  game.game_start
 end
