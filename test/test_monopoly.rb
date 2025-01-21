30efeb9bed848a229cffc7090d1d499f9adbfd56
JunyIIIChen <cjy799255537@gmail.com>
2025-01-22 02:45:12 +1100

    test failed-player pass Go get $1-refactoring code

---
+++ test/test_monopoly.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+require 'json'
+require_relative '../monopoly'
+
+class TestMonopoly < Minitest::Test
+  def setup
+    @board_file = 'board.json'
+    @dice_rolls_file = 'rolls_1.json'
+    @game = Monopoly.new(@board_file, @dice_rolls_file)
+  end
+
+  def test_initialize_game
+    #test players have been generated
+    player_names = @game.players.map{ |player| player.name }
+
+    assert_equal "Peter", player_names[0]
+    assert_equal "Billy", player_names[1]
+    assert_equal "Charlotte", player_names[2]
+    assert_equal "Sweedal", player_names[3]
+    assert_equal 4, @game.players.size
+  end
+
+
+  def test_pass_go_get_one_dollar
+    player = @game.players[0]
+    initial_money = player.money
+
+    # assert_equal 16, player.money
+
+    player.position = @game.board.locations.size - 1
+
+    @game.take_turn_to_move(player,3)
+
+    assert_equal 17, player.money
+  end
+  
+end
